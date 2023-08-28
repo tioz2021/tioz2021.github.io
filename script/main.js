3cf91ca76ec9e34b3d594485261348e379093891
tioz2021 <jenya25life@gmail.com>
2023-08-28 19:38:29 +0300

    video adaptiv?

--- script/main.js
+++ script/main.js
@@ -54,3 +54,23 @@ window.onload = function () {
     document.querySelector("main").style.opacity = "1";
   }, 1500);
 };
+
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+if (window.matchMedia("(prefers-reduced-motion)").matches) {
+  vid.removeAttribute("autoplay");
+  vid.pause();
+  pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener("ended", function () {
+  // only functional if "loop" is removed
+  vid.pause();
+  // to capture IE10
+  vidFade();
+});
