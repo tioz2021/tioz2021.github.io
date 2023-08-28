840e372f0f1a49f820bfc651e537a49a675a8549
tioz2021 <jenya25life@gmail.com>
2023-08-28 19:42:28 +0300

    ff

--- script/main.js
+++ script/main.js
@@ -49,28 +49,29 @@
 // wow init ( animation ) and preloader
 window.onload = function () {
   new WOW().init();
+
   setTimeout(() => {
     document.querySelector(".preloader").classList.add("disable");
     document.querySelector("main").style.opacity = "1";
   }, 1500);
+
+  var vid = document.getElementById("bgvid");
+  var pauseButton = document.querySelector("#polina button");
+
+  if (window.matchMedia("(prefers-reduced-motion)").matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+
+  function vidFade() {
+    vid.classList.add("stopfade");
+  }
+
+  vid.addEventListener("ended", function () {
+    // only functional if "loop" is removed
+    vid.pause();
+    // to capture IE10
+    vidFade();
+  });
 };
-
-var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("#polina button");
-
-if (window.matchMedia("(prefers-reduced-motion)").matches) {
-  vid.removeAttribute("autoplay");
-  vid.pause();
-  pauseButton.innerHTML = "Paused";
-}
-
-function vidFade() {
-  vid.classList.add("stopfade");
-}
-
-vid.addEventListener("ended", function () {
-  // only functional if "loop" is removed
-  vid.pause();
-  // to capture IE10
-  vidFade();
-});
