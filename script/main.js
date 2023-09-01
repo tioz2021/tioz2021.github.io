8cc399cf841db06597cd62abfb88814d5364ffec
tioz2021 <jenya25life@gmail.com>
2023-09-01 19:27:53 +0300

    fix

--- script/main.js
+++ script/main.js
@@ -11,6 +11,13 @@
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   });
+
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+  window.addEventListener("resize", appHeight);
+  appHeight();
 })();
 
 // sliders init and config
