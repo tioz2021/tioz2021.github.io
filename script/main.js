d4dba41ca426d1f521c9a6fecabe105885d6e2b2
tioz2021 <jenya25life@gmail.com>
2023-09-01 19:39:57 +0300

    fix

--- script/main.js
+++ script/main.js
@@ -3,24 +3,18 @@ new WOW().init();
 
 // fs 100vh fixing
 (() => {
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-  // We listen to the resize event
-  window.addEventListener("resize", () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  });
-
-  const appHeight = () => {
-    const doc = document.documentElement;
-    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-  };
-  window.addEventListener("resize", appHeight);
-  appHeight();
+  // let vh = window.innerHeight * 0.01;
+  // document.documentElement.style.setProperty("--vh", `${vh}px`);
+  // window.addEventListener("resize", () => {
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty("--vh", `${vh}px`);
+  // });
+  // const appHeight = () => {
+  //   const doc = document.documentElement;
+  //   doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  // };
+  // window.addEventListener("resize", appHeight);
+  // appHeight();
 })();
 
 // sliders init and config
