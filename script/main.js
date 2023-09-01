538d73d166d56ec0fe70a1f7d145847371ba8128
tioz2021 <jenya25life@gmail.com>
2023-09-01 19:45:10 +0300

    test

--- script/main.js
+++ script/main.js
@@ -9,12 +9,12 @@ new WOW().init();
   //   let vh = window.innerHeight * 0.01;
   //   document.documentElement.style.setProperty("--vh", `${vh}px`);
   // });
-  // const appHeight = () => {
-  //   const doc = document.documentElement;
-  //   doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-  // };
-  // window.addEventListener("resize", appHeight);
-  // appHeight();
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+  window.addEventListener("resize", appHeight);
+  appHeight();
 })();
 
 // sliders init and config
