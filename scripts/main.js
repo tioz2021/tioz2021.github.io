48254efc6f53db8504a78e62d2de395f7bf972be
tioz2021 <jenya25life@gmail.com>
2023-12-24 11:44:21 +0200

    all fixed ?

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 // init animation
 new WOW().init();
 
+// init aos
+AOS.init();
+
 // sliders
 (() => {
   const swiper1 = new Swiper(".swiper1", {
@@ -358,3 +361,34 @@ new WOW().init();
     requestAnimationFrame(animateScroll);
   }
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+  var blocks = document.querySelectorAll(".block");
+
+  function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function handleScroll() {
+    blocks.forEach(function (block) {
+      if (isInViewport(block)) {
+        block.classList.add("transformed");
+      } else {
+        block.classList.remove("transformed");
+      }
+    });
+  }
+
+  // Обработка события скролла
+  window.addEventListener("scroll", handleScroll);
+
+  // Обработка события загрузки страницы для первичной проверки видимости
+  window.addEventListener("load", handleScroll);
+});
