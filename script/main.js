9b185e3ae0dd2bc1f1fd9a6eef1b5153d6384d73
tioz2021 <jenya25life@gmail.com>
2024-04-09 19:09:37 +0300

    livirik day3 work ( s2, s3 )

--- script/main.js
+++ script/main.js
@@ -1,3 +1,12 @@
 (() => {
-  //
+  var s3_swiper = new Swiper(".s3__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    effect: "fade",
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 })();
