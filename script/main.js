074b769eea943993a05339e1b56d5d14686d8407
tioz2021 <jenya25life@gmail.com>
2024-04-10 21:48:03 +0300

    llivirik day4 work (s4, s5, footer)

--- script/main.js
+++ script/main.js
@@ -10,3 +10,34 @@
     },
   });
 })();
+
+// auto change height on textarea
+// (() => {
+//   const textarea = document.querySelector("textarea");
+//   if (textarea) textarea.addEventListener("input", autoResize);
+//   function autoResize() {
+//     const textarea = this;
+//     // var charLimitMessage = document.getElementById("charLimitMessage");
+//     var maxLength = textarea.getAttribute("maxlength");
+//     var currentLength = textarea.value.length;
+
+//     if (currentLength > maxLength) {
+//       // charLimitMessage.textContent = "Превышен лимит символов (максимум 5000).";
+//       textarea.value = textarea.value.substring(0, maxLength);
+//       textarea.style.height = "auto";
+//       textarea.style.height =
+//         textarea.scrollHeight +
+//         textarea.offsetHeight -
+//         textarea.clientHeight +
+//         "px";
+//     } else {
+//       // charLimitMessage.textContent = "";
+//       textarea.style.height = "auto";
+//       textarea.style.height =
+//         textarea.scrollHeight +
+//         textarea.offsetHeight -
+//         textarea.clientHeight +
+//         "px";
+//     }
+//   }
+// })();
