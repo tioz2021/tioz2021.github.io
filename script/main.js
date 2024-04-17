c39329d96e4aaddd7989a6f6028a7820593a6497
tioz2021 <jenya25life@gmail.com>
2024-04-17 18:51:36 +0300

    day 10 work finish

--- script/main.js
+++ script/main.js
@@ -1,3 +1,47 @@
+// other
+(() => {
+  // init AOS
+  AOS.init();
+
+  // preloader
+  document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+      if (document.querySelector(".preloader"))
+        document.querySelector(".preloader").classList.add("disabled");
+      // document.querySelector(".main-wrp").classList.add("active");
+    }, 2250);
+
+    // card lottie animation timing
+    if (
+      document.querySelector(".card-box__decoration-img--one lottie-player")
+    ) {
+      document
+        .querySelector(".card-box__decoration-img--one lottie-player")
+        .play();
+
+      setTimeout(() => {
+        document
+          .querySelector(".card-box__decoration-img--two lottie-player")
+          .play();
+      }, 1000);
+    }
+
+    // fs main page animation timing
+    if (document.querySelector(".s1__decoration-bg lottie-player")) {
+      setTimeout(() => {
+        document.querySelector(".s1__decoration-bg--left lottie-player").play();
+      }, 2350);
+
+      setTimeout(() => {
+        document
+          .querySelector(".s1__decoration-bg--right lottie-player")
+          .play();
+      }, 2850);
+    }
+  });
+})();
+
+// slider?
 (() => {
   var s3_swiper = new Swiper(".s3__swiper", {
     slidesPerView: 1,
@@ -16,30 +60,91 @@
 
     const scrollTop = window.scrollY || window.pageYOffset;
 
-    console.log(scrollTop);
+    // console.log(scrollTop);
 
     // s2 scroller 1
     if (scrollTop >= 5350 && scrollTop <= 5650) {
       if (scrollingBlock.classList.contains("fixed") == false) {
-        scrollingBlock.classList.add("fixed");
-        scrollingBlock.style.top = "-1412rem";
+        // scrollingBlock.classList.add("fixed");
+        // scrollingBlock.style.top = "-1412rem";
       }
     } else if (scrollTop <= 5350) {
       if (scrollingBlock.classList.contains("fixed") == true) {
-        scrollingBlock.classList.remove("fixed");
-        scrollingBlock.style.top = "3949rem";
+        // scrollingBlock.classList.remove("fixed");
+        // scrollingBlock.style.top = "3949rem";
       }
     } else if (scrollTop >= 5650) {
       if (scrollingBlock.classList.contains("fixed") == true) {
-        scrollingBlock.classList.remove("fixed");
-        scrollingBlock.style.top = "4249rem";
+        // scrollingBlock.classList.remove("fixed");
+        // scrollingBlock.style.top = "4249rem";
       }
     }
-    if (scrollTop >= 5351 && scrollTop <= 5450) s3_swiper.slideTo(0, 1000);
-    else if (scrollTop >= 5451 && scrollTop <= 5550) s3_swiper.slideTo(1, 1000);
+    // if (scrollTop >= 5351 && scrollTop <= 5450) s3_swiper.slideTo(0, 1000);
+    // else if (scrollTop >= 5451 && scrollTop <= 5550) s3_swiper.slideTo(1, 1000);
   }
 
   window.addEventListener("scroll", scroll_slider_block);
+
+  // const scrollingBlock = document.querySelector(".s3__swiper-pagination");
+  // const elementChanged = document.querySelector(".s3");
+
+  // function scroll_slider_block() {
+  //   if (!scrollingBlock || !elementChanged) return;
+
+  //   const observer = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry) => {
+  //       if (
+  //         entry.isIntersecting &&
+  //         !elementChanged.classList.contains("fixed")
+  //       ) {
+  //         const pos = window.scrollY;
+  //         console.log("fixed");
+  //         // Блок попал в область видимости и еще не зафиксирован
+  //         elementChanged.classList.add("fixed");
+  //         // elementChanged.style.top = "-1412rem";
+  //         elementChanged.style.top = "-1451rem";
+
+  //         const scrollCallbackTop = () => {
+  //           if (window.scrollY <= pos - 400) {
+  //             console.log("unFixed top?");
+
+  //             elementChanged.classList.remove("fixed");
+  //             elementChanged.style.top = "3949rem";
+
+  //             window.removeEventListener("scroll", scrollCallbackTop);
+  //           }
+  //         };
+
+  //         const scrollCallbackBot = () => {
+  //           if (window.scrollY >= pos + 400) {
+  //             console.log("unFixed bot?");
+
+  //             elementChanged.classList.remove("fixed");
+  //             elementChanged.style.top = "4249rem";
+
+  //             window.removeEventListener("scroll", scrollCallbackBot);
+  //           }
+  //         };
+
+  //         // if (window.scrollY >= pos + 400) {
+  //         //   console.log("unFixed bot?");
+  //         //   // elementChanged.style.top = "3949rem";
+  //         //   // elementChanged.classList.remove("fixed");
+  //         //   // window.removeEventListener("scroll", scrollCallback);
+  //         // }
+
+  //         // Добавляем обработчик прокрутки
+  //         window.addEventListener("scroll", scrollCallbackTop);
+  //         window.addEventListener("scroll", scrollCallbackBot);
+  //       }
+  //     });
+  //   });
+
+  //   observer.observe(scrollingBlock);
+  // }
+
+  // // Вызываем функцию при загрузке страницы
+  // window.addEventListener("load", scroll_slider_block);
 })();
 
 // box selected
@@ -148,51 +253,6 @@
   });
 })();
 
-// (() => {
-//   document.addEventListener("DOMContentLoaded", function () {
-//     let popup = "";
-//     const openBtn = document.querySelector(".openBtn");
-
-//     if (!openBtn) {
-//       return false;
-//     }
-
-//     if (openBtn.classList.contains("openBtn1")) {
-//       popup = document.querySelector(".popup1");
-//     } else if (openBtn.classList.contains("openBtn2")) {
-//       popup = document.querySelector(".popup2");
-//     } else {
-//       popup = document.querySelector(".popup-form");
-//     }
-
-//     const popupContent = popup.querySelector(".popup-content");
-//     const closeBtn = popup.querySelectorAll(".closeBtn");
-
-//     openBtn.addEventListener("click", function (evt) {
-//       evt.preventDefault();
-//       popup.classList.add("open");
-//       popupContent.classList.add("open");
-//     });
-
-//     closeBtn.forEach((e) => {
-//       e.addEventListener("click", function () {
-//         popup.classList.remove("open");
-//         popupContent.classList.remove("open");
-//       });
-//     });
-
-//     document.addEventListener("click", function (event) {
-//       if (
-//         !popupContent.contains(event.target) &&
-//         !openBtn.contains(event.target)
-//       ) {
-//         popup.classList.remove("open");
-//         popupContent.classList.remove("open");
-//       }
-//     });
-//   });
-// })();
-
 // toggled cardHeader
 (() => {
   const lists = document.querySelectorAll(".card-header__bottom-list");
@@ -232,33 +292,66 @@
   }
 })();
 
-// auto change height on textarea
-// (() => {
-//   const textarea = document.querySelector("textarea");
-//   if (textarea) textarea.addEventListener("input", autoResize);
-//   function autoResize() {
-//     const textarea = this;
-//     // var charLimitMessage = document.getElementById("charLimitMessage");
-//     var maxLength = textarea.getAttribute("maxlength");
-//     var currentLength = textarea.value.length;
+// animations
+(() => {
+  // Создаем новый экземпляр Intersection Observer
+  const observer = new IntersectionObserver(
+    (entries) => {
+      // Перебираем все записи (entries)
+      entries.forEach((entry) => {
+        // Если блок стал видимым и находится в зоне видимости больше 1 секунды
+        if (
+          entry.isIntersecting &&
+          entry.intersectionRatio >= 0.1 &&
+          entry.time >= 100
+        ) {
+          console.log(entry);
+          // Запускаем анимацию или выполняем нужные действия
 
-//     if (currentLength > maxLength) {
-//       // charLimitMessage.textContent = "Превышен лимит символов (максимум 5000).";
-//       textarea.value = textarea.value.substring(0, maxLength);
-//       textarea.style.height = "auto";
-//       textarea.style.height =
-//         textarea.scrollHeight +
-//         textarea.offsetHeight -
-//         textarea.clientHeight +
-//         "px";
-//     } else {
-//       // charLimitMessage.textContent = "";
-//       textarea.style.height = "auto";
-//       textarea.style.height =
-//         textarea.scrollHeight +
-//         textarea.offsetHeight -
-//         textarea.clientHeight +
-//         "px";
-//     }
-//   }
-// })();
+          if (entry.target.classList.contains("s2__main-img--center") == true) {
+            entry.target.classList.add("anim");
+            setTimeout(() => {
+              document.querySelector(".s2__decor--left lottie-player").play();
+            }, 750);
+            setTimeout(() => {
+              document.querySelector(".s2__decor--right lottie-player").play();
+            }, 850);
+          }
+
+          if (entry.target.classList.contains("s3__main-title-img") == true) {
+            entry.target.classList.add("anim");
+            setTimeout(() => {
+              document.querySelector(".s3__decor--left lottie-player").play();
+            }, 150);
+            setTimeout(() => {
+              document.querySelector(".s3__decor--right lottie-player").play();
+            }, 250);
+          }
+
+          if (entry.target.classList.contains("wrp") == true) {
+            entry.target.classList.add("anim");
+          }
+
+          // После того, как анимация выполнена, можно отключить наблюдение, если оно больше не нужно
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 1, delay: 100 }
+  ); // Устанавливаем порог пересечения 100% и задержку 1 секунда
+
+  // Список селекторов для анимации
+  const selectors = [
+    ".s2__main-img--center",
+    ".s3__main-title-img",
+    ".footer__decoration-box .wrp",
+  ];
+
+  selectors.forEach((selector) => {
+    // Находим блок для текущего селектора
+    const blockToAnimate = document.querySelector(selector);
+    // Начинаем наблюдение за блоком
+    if (!blockToAnimate) return;
+    observer.observe(blockToAnimate);
+  });
+})();
