ea65b3af3fcf87573b31c297f80c34f294d86be8
tios23 <jenya25life@gmail.com>
2024-05-05 11:56:35 +0300

    test pull

--- script/main.js
+++ script/main.js
@@ -189,3 +189,43 @@ var swiper_def_slider2 = new Swiper(swiper_def_slider_element2, {
   svg += "</svg>";
   // console.log(svg);
 })();
+
+// acardion
+(() => {
+  // Получаем все элементы аккордеона
+  const accordionItems = document.querySelectorAll(".accordion__item");
+
+  // Добавляем обработчик события для каждого элемента аккордеона
+  accordionItems.forEach((item) => {
+    const header = item.querySelector(".accordion__header");
+    const content = item.querySelector(".accordion__content");
+
+    header.addEventListener("click", () => {
+      // Переключаем класс 'active' для заголовка текущего элемента
+      header.classList.toggle("active");
+
+      // Закрываем все другие элементы аккордеона
+      accordionItems.forEach((otherItem) => {
+        if (otherItem !== item) {
+          otherItem
+            .querySelector(".accordion__header")
+            .classList.remove("active");
+          otherItem.querySelector(".accordion__content").style.maxHeight = "0";
+          otherItem.querySelector(".accordion__content").style.paddingBottom =
+            "0px";
+        }
+      });
+
+      // Устанавливаем максимальную высоту для текущего элемента в rem
+      if (header.classList.contains("active")) {
+        // Получаем высоту текста внутри .accordion__content и устанавливаем в rem
+        const textHeight = content.scrollHeight;
+        content.style.maxHeight = `${textHeight + 24}px`;
+        content.style.paddingBottom = "24px";
+      } else {
+        content.style.maxHeight = "0";
+        content.style.paddingBottom = "0px";
+      }
+    });
+  });
+})();
